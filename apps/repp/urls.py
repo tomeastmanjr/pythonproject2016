910a9aa12ef3a59e0cb2d6441350a4d6c1ea1752
ryanybrennan <ryanybrennan@msn.com>
2016-08-25 17:20:57 -0700

    testing stuff

--- apps/repp/urls.py
+++ apps/repp/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from . import views
+import re
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^add$', views.add, name="add"),
-    url(r'^create$', views.create, name="create"),
+    url(r'^create_loan/(?P<id>\d+)$', views.create, name="create"),
     url(r'^show$', views.show, name="show"),
     url(r'^update$', views.update, name="update"),
     url(r'^test$', views.test, name="test"),
     url(r'^about$', views.about, name="about"),
-    url(r'^logout$', views.logout, name="logout")
+    url(r'^logout$', views.logout, name="logout"),
+    url(r'^ryan_test$', views.ryan_test, name = "ryan_test")
     ]
--- apps/repp/views.py
+++ apps/repp/views.py
@@ -10,7 +10,7 @@ def index(request):
 
     user = User.objects.get(id=request.session["id"])
     context = {
-    #     "trips":Trip.objects.all()[:100],
+     "users":User.objects.all()[:100],
     #     "me": (Trip.objects.filter(creator=user) | Trip.objects.filter(travelers__id=user.id)).distinct(),
     #     "notme": Trip.objects.all().exclude(creator=user).exclude(travelers__id=user.id),
     #     "name": user.name
@@ -34,23 +34,21 @@ def add(request):
 
     return render(request, 'repp/add.html')
 
-def create(request):
-    # if 'id' not in request.session:
-    #     return redirect(reverse('loginreg:index'))
-    #
-    # if request.POST["trip_begin"] and request.POST["trip_end"]:
+def ryan_test(request):
+    return render(request, 'repp/ryan_test.html')
+def create(request, id):
+    if 'id' not in request.session:
+        return redirect(reverse('loginreg:index'))
+    
     #     user = User.objects.get(id=request.session['id'])
     #     description = request.POST["description"]
     #     trip_begin = datetime.strptime(request.POST["trip_begin"], '%Y-%m-%d')
     #     trip_end = datetime.strptime(request.POST["trip_end"], '%Y-%m-%d')
     #     destination=request.POST["destination"]
     #
-    #     if len(description) >1 and len(description) >1 and trip_begin > datetime.now() and trip_end > trip_begin:
-    #         trip = Trip.objects.create(description=description, trip_begin=trip_begin, trip_end=trip_end, destination=destination, creator=user)
-    #         return redirect(reverse('repp:show', kwargs={"trip_id":trip.id}))
-    # else:
-    #     messages.add_message(request, messages.SUCCESS, 'Try again')
-    #     return render(request, 'repp/add.html')
+
+    return redirect(reverse('repp:ryan_test'))
+    # kwargs={"trip_id":trip.id}))
     pass
 
 def update(request):
